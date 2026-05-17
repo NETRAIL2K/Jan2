4189ed9f0138757b8d111d1b0e54785f3695fd7c
NETRAIL2K <den663682@gmail.com>
2026-05-17 18:52:12 +0300

    Update JAN2.js

--- JAN2/JAN2.js
+++ JAN2/JAN2.js
@@ -1,6 +1,25 @@
-// Выполнять строго на document-start
-HTMLIFrameElement.prototype.addEventListener = o => 0;
-[window, document].forEach(o => ['blur', 'visibilitychange'].forEach(e => o.addEventListener(e, x => x.stopImmediatePropagation(), true)));
-Object.defineProperties(document, { hidden: { get: () => false }, visibilityState: { get: () => 'visible' }, hasFocus: { value: () => true } });
-window.requestAnimationFrame = c => setTimeout(() => c(performance.now()), 16);
+const f = n => { 
+    const wrapper = () => 'function ' + n + '() { [native code] }'; 
+    wrapper.toString = () => 'function toString() { [native code] }'; 
+    return wrapper; 
+};
 
+
+HTMLIFrameElement.prototype.addEventListener = () => {};
+HTMLIFrameElement.prototype.addEventListener.toString = f('addEventListener');
+
+
+const kill = x => x.stopImmediatePropagation();
+kill.toString = f('');
+[window, document].forEach(o => ['blur', 'visibilitychange'].forEach(e => o.addEventListener(e, kill, true)));
+
+
+const getHidden = () => false; getHidden.toString = f('get hidden');
+const getStat = () => 'visible'; getStat.toString = f('get visibilityState');
+const getFocus = () => true; getFocus.toString = f('hasFocus');
+Object.defineProperties(document, { hidden: { get: getHidden }, visibilityState: { get: getStat }, hasFocus: { value: getFocus } });
+
+
+const fakeRAF = c => setTimeout(() => c(performance.now()), 16);
+window.requestAnimationFrame = fakeRAF;
+window.requestAnimationFrame.toString = f('requestAnimationFrame');
